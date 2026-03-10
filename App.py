6bd5f55227542c6aa72be6195a1108ecc79bf788
Anoop Chaudhari <anoopchaudhari263@gmail.com>
2026-03-10 23:42:18 +0530

    Delete App.py

--- App.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-
-st.title("Rajesh Portfolio")
-st.image("your_photo.png")
-st.write("Welcome to my portfolio website!")
-st.markdown("[GitHub Repo](https://github.com/raxx21/rajesh-portfolio)")
