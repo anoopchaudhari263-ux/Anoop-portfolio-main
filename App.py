ddf8a638caa77e8d370a3d4f59b58aba38eafb56
Anoop Chaudhari <anoopchaudhari263@gmail.com>
2026-03-10 23:39:21 +0530

    Create App.py

---
+++ App.py
@@ -0,0 +1,6 @@
+import streamlit as st
+
+st.title("Rajesh Portfolio")
+st.image("your_photo.png")
+st.write("Welcome to my portfolio website!")
+st.markdown("[GitHub Repo](https://github.com/raxx21/rajesh-portfolio)")
